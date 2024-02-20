8568d5c82ebe4e9479e0fee156f5ee2ec9f86eff
Kelvin Hoover <kelvin.hoover@hotmail.com>
2024-02-19 19:39:56 -0800

    Checkpoint work

--- Src/TicketShare/TicketShare.sdk/Data/StaticData.cs
+++ Src/TicketShare/TicketShare.sdk/Data/StaticData.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using TicketShare.sdk.Tools;
-using Toolbox.Tools;
-using Toolbox.Extensions;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TicketShare.sdk.Tools;
+//using Toolbox.Tools;
+//using Toolbox.Extensions;
 
-namespace TicketShare.sdk;
+//namespace TicketShare.sdk;
 
-public static class StaticData
-{
-    public static string GetIntro() => AssemblyResource.GetResourceString("TicketShare.sdk.Data.Intro.md", typeof(StaticData));
-    public static MarkdownDoc GetIntroDoc() => GetIntro().ToMarkdownDoc();
+//public static class StaticData
+//{
+//    public static string GetIntro() => AssemblyResource.GetResourceString("TicketShare.sdk.Data.Intro.md", typeof(StaticData));
+//    public static MarkdownDoc GetIntroDoc() => GetIntro().ToMarkdownDoc();
 
-    public static string GetIntroHtml()
-    {
-        string html = AssemblyResource.GetResourceString("TicketShare.sdk.Data.Intro.html", typeof(StaticData));
+//    public static string GetIntroHtml()
+//    {
+//        string html = AssemblyResource.GetResourceString("TicketShare.sdk.Data.Intro.html", typeof(StaticData));
 
-        if (html.IsEmpty()) return html;
+//        if (html.IsEmpty()) return html;
 
-        var replacements = ((string searchFor, string replaceWith)[])[
-            ("src=\"SharringTickets.webp\"", "src=\"data:image/webp;base64," + GetImage("SharringTickets").GetBase64() + "\""),
-            ("src=\"Fans.webp\"", "src=\"data:image/webp;base64," + GetImage("Fans").GetBase64() + "\""),
-            ("src=\"FansTrading.webp\"", "src=\"data:image/webp;base64," + GetImage("FansTrading").GetBase64() + "\""),
-            ("src=\"FanHappy.webp\"", "src=\"data:image/webp;base64," + GetImage("FanHappy").GetBase64() + "\""),
-        ];
+//        var replacements = ((string searchFor, string replaceWith)[])[
+//            ("src=\"SharringTickets.webp\"", "src=\"data:image/webp;base64," + GetImage("SharringTickets").GetBase64() + "\""),
+//            ("src=\"Fans.webp\"", "src=\"data:image/webp;base64," + GetImage("Fans").GetBase64() + "\""),
+//            ("src=\"FansTrading.webp\"", "src=\"data:image/webp;base64," + GetImage("FansTrading").GetBase64() + "\""),
+//            ("src=\"FanHappy.webp\"", "src=\"data:image/webp;base64," + GetImage("FanHappy").GetBase64() + "\""),
+//        ];
 
-        foreach(var replacement in replacements)
-        {
-            html = html.Replace(replacement.searchFor, replacement.replaceWith);
-        }
+//        foreach(var replacement in replacements)
+//        {
+//            html = html.Replace(replacement.searchFor, replacement.replaceWith);
+//        }
 
-        return html;
-    }
+//        return html;
+//    }
 
-    private static StaticImage GetImage(string resourceId) => new StaticImage
-    {
-        Name = resourceId,
-        Data = AssemblyResource.GetResourceBytes($"TicketShare.sdk.Data.{resourceId}.webp", typeof(StaticData))!
-    };
-}
+//    private static StaticImage GetImage(string resourceId) => new StaticImage
+//    {
+//        Name = resourceId,
+//        Data = AssemblyResource.GetResourceBytes($"TicketShare.sdk.Data.{resourceId}.webp", typeof(StaticData))!
+//    };
+//}
