fb6334e28f46481bc48a1ae80ab26108455e0008
Kelvin Hoover <kelvin.hoover@hotmail.com>
2025-01-07 01:21:40 -0800

    Moved project folders up one level and started working on the Account

---
+++ Src/TicketShare/TicketShareWeb/Application/UITool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using TicketShare.sdk;
+using Fluent = Microsoft.FluentUI.AspNetCore.Components;
+
+namespace TicketShareWeb.Application;
+
+public static class UITool
+{
+    public static IReadOnlyList<Fluent.Option<string>> ToFluentOption<TEnum>() where TEnum : struct, Enum
+    {
+        var list = Enum.GetNames<TEnum>()
+           .OrderBy(x => x)
+           .Select(x => new Fluent.Option<string> { Value = x, Text = x })
+           .ToImmutableArray();
+
+        return list;
+    }
+
+    public static IReadOnlyList<Fluent.Option<string>> ValidRoleTypes { get; } = ToFluentOption<RoleType>();
+
+    public static IReadOnlyList<Fluent.Option<string>> ValidContactTypes = ToFluentOption<ContactType>();
+}
