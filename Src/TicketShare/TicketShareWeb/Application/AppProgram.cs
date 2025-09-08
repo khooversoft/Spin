3da287fd037647d08091343be6c6ee5107f47c00
Kelvin Hoover <kelvin.hoover@hotmail.com>
2025-09-07 23:40:27 -0700

    Build, deploy, start/restart

--- Src/TicketShare/TicketShareWeb/Application/AppProgram.cs
+++ Src/TicketShare/TicketShareWeb/Application/AppProgram.cs
@@ -3,5 +3,5 @@
 public class AppProgram
 {
     public static string ServiceName = "TicketShareWeb";
-    public static string ServiceVersion = "1.0.9";
+    public static string ServiceVersion = "1.0.10";
 }
