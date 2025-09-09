8da9fca28f7799631108418806274af9203be95b
Kelvin Hoover <kelvin.hoover@hotmail.com>
2025-09-09 08:26:20 -0700

    Yaml #3

--- Src/RaceAlive/RaceAliveWeb/Application/AppProgram.cs
+++ Src/RaceAlive/RaceAliveWeb/Application/AppProgram.cs
@@ -2,6 +2,6 @@
 
 public class AppProgram
 {
-    public static string ServiceName = "TicketShareWeb";
-    public static string ServiceVersion = "1.0.10";
+    public static string ServiceName = "RaceAliveWeb";
+    public static string ServiceVersion = "1.0.1";
 }
