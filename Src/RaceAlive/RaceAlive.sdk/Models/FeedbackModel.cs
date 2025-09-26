7d947f9234fe014b21814640574856ac7909d1e7
Kelvin Hoover <kelvin.hoover@hotmail.com>
2025-09-25 22:42:51 -0700

    Did some cleanup, added about and feedback

---
+++ Src/RaceAlive/RaceAlive.sdk/Models/FeedbackModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolbox.Tools;
+using Toolbox.Types;
+
+namespace RaceAlive.sdk.Models;
+
+public class FeedbackModel
+{
+    public string? Subject { get; set; }
+    public string Text { get; set; } = null!;
+    public string? ContactEmail { get; set; }
+
+    public static IValidator<FeedbackModel> Validator { get; } = new Validator<FeedbackModel>()
+        .RuleFor(x => x.Text).NotEmpty()
+        .Build();
+}
+
+public static class FeedbackModelExtensions
+{
+    public static Option<IValidatorResult> Validate(this FeedbackModel model) => FeedbackModel.Validator.Validate(model);
+}
